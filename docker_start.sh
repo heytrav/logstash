ebc1ace090d73b3ca06705636889dc26ceff4eaa
Travis Holton <travis@ideegeo.com>
2015-02-10 19:16:40 +1100

    Replace with actual environment variable

--- docker_start.sh
+++ docker_start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 
-sed -i 's/ELASTICSEARCH_TRANSPORT/$ELASTICSEARCH_TRANSPORT/g' /etc/logstash/conf.d/syslog.conf
+sed -i 's/ELASTICSEARCH_TRANSPORT_SERVICE_HOST/$ELASTICSEARCH_TRANSPORT_SERVICE_HOST/g' /etc/logstash/conf.d/syslog.conf
+sed -i 's/ELASTICSEARCH_TRANSPORT_SERVICE_PORT/$ELASTICSEARCH_TRANSPORT_SERVICE_PORT/g' /etc/logstash/conf.d/syslog.conf
+sed -i 's/LUMBERJACK_SERVICE_PORT/$LUMBERJACK_SERVICE_PORT/g' /etc/logstash/conf.d/syslog.conf
 
 
 /usr/bin/supervisord --no-daemon
