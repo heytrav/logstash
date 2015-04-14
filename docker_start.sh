ab54ba94101c369330145fbca1895be212823d00
Travis Holton <travis@ideegeo.com>
2015-04-14 18:47:04 +1200

    map to running elasticsearch-lb service

--- docker_start.sh
+++ docker_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sed -i 's/ELASTICSEARCH_TRANSPORT_SERVICE_HOST/'"$ELASTICSEARCH_TRANSPORT_SERVICE_HOST"'/g' /etc/logstash/conf.d/syslog.conf
+sed -i 's/ELASTICSEARCH_LB_SERVICE_HOST/'"$ELASTICSEARCH_LB_SERVICE_HOST"'/g' /etc/logstash/conf.d/syslog.conf
 sed -i 's/ELASTICSEARCH_TRANSPORT_SERVICE_PORT/'"$ELASTICSEARCH_TRANSPORT_SERVICE_PORT"'/g' /etc/logstash/conf.d/syslog.conf
 sed -i 's/LUMBERJACK_SERVICE_PORT/'"$LUMBERJACK_SERVICE_PORT"'/g' /etc/logstash/conf.d/syslog.conf
 
