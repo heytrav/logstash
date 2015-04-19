c717feb2278b41bbce641eade12044c9a3e4956d
Travis Holton <travis@ideegeo.com>
2015-04-19 12:43:20 +1200

    take out search/replace

--- docker_start.sh
+++ docker_start.sh
@@ -2,7 +2,6 @@
 
 
 sed -i 's/LUMBERJACK_SERVICE_PORT/'"$LUMBERJACK_SERVICE_PORT"'/g' /etc/logstash/conf.d/syslog.conf
-sed -i 's/ELASTICSEARCH_SERVICE_HOST/'"$ELASTICSEARCH_SERVICE_HOST"'/g' /etc/logstash/conf.d/syslog.conf
 
 
 /usr/bin/supervisord
