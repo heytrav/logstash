8f944cfcb604768a59399cbd56fb8843dc06c295
Travis Holton <travis@ideegeo.com>
2015-05-11 11:28:36 +1200

    allow ssh login

--- docker_start.sh
+++ docker_start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 
-sed -i 's/LUMBERJACK_SERVICE_PORT/'"$LUMBERJACK_SERVICE_PORT"'/g' /etc/logstash/conf.d/syslog.conf
-
+if [ -n "$SSH_PASSWORD" ]; then
+    echo "Configuring ssh: setting root password to ${SSH_PASSWORD}"
+    echo "root:$SSH_PASSWORD" | chpasswd
+    sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+    #grep "PermitRootLogin" /etc/ssh/sshd_config
+    /usr/sbin/sshd
+    # Append Docker environment variables, otherwise they are not accessable to ssh users
+    # in any way
+    env | grep _ >> /etc/environment
+fi
 
 /usr/bin/supervisord
